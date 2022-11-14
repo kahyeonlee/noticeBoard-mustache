f7c420abf0d01bc6c42fc31bafa7ed21197fbb2f
kahyun Lee <gmeoqq@gmail.com>
2022-11-14 15:04:18 +0900

    HospitalRepository 메소드 생성하기 (between까지)

---
+++ src/main/java/com/mustache/noticeboard/repository/HospitalRepository.java
@@ -0,0 +1,19 @@
+package com.mustache.noticeboard.repository;
+
+
+import com.mustache.noticeboard.domain.entity.Hospital;
+import com.mustache.noticeboard.domain.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
+
+    List<Hospital> findByBusinessTypeNameIn(List<String> inClues);
+    List<Hospital> findByRoadNameAddressContaining(String keyword);
+    List<Hospital> findByHospitalNameStartsWith(String keyword);
+    List<Hospital> findByHospitalNameEndsWith(String keyword);
+
+    List<Hospital> findByPatientRoomCountBetween(int startRoomCount, int endRoomCount);
+
+}
