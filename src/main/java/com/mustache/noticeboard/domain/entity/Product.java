d2e863b9d6773709ed739efdefd5a947a0cc63d2
kahyun Lee <gmeoqq@gmail.com>
2022-11-14 11:20:47 +0900

    product 테이블 생성

---
+++ src/main/java/com/mustache/noticeboard/domain/entity/Product.java
@@ -0,0 +1,30 @@
+package com.mustache.noticeboard.domain.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
